173291a5b73cb4f5b79c17ba33737c17e368a4be
maxiangpan <maxiangpan@126.com>
2018-04-17 22:14:10 +0800

    new

---
+++ src/main/scala/RDD.scala
@@ -0,0 +1,13 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object RDD {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setAppName("wocao").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+
+    sc.parallelize(Array(1,2,4,5)).repartition(1)
+
+  }
+}
